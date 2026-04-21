50c5e04161e0bb74c00b4b3ec91424455f01451e
mailong2401 <gtmailong@gmail.com>
2026-04-21 23:55:09 +0700

    update

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,14 +1,52 @@
 import LiquidGlass from "liquid-glass-react";
+import { useEffect, useState } from "react";
 
 const menuItems = ["Home", "About", "Blogs", "Projects", "Tools"];
 
 export default function Header() {
 
+  const [show, setShow] = useState(true);
+
+  useEffect(() => {
+    let lastScrollY = window.scrollY;
+
+    const handleScroll = () => {
+      const currentScrollY = window.scrollY;
+
+      if (currentScrollY > lastScrollY) {
+        setShow(false);
+      } else {
+        setShow(true);
+      }
+
+      lastScrollY = currentScrollY;
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
 
   return (
-    <div
-    >
-      <LiquidGlass
+    <
+      >
+      {!show && <LiquidGlass displacementScale={100}
+        blurAmount={0.2}
+        saturation={140}
+        aberrationIntensity={2}
+        elasticity={0.2}
+        cornerRadius={32}
+        mode="standard"
+        padding="12px 24px"
+        style={{
+          position: "fixed",
+          top: "1%",
+          left: "50%",
+        }}>
+        <div className="w-50 h-2"></div>
+      </LiquidGlass>}
+      {show && <LiquidGlass
         displacementScale={100}
         blurAmount={0.2}
         saturation={140}
@@ -39,7 +77,7 @@ export default function Header() {
             </a>
           ))}
         </div>
-      </LiquidGlass>
-    </div>
+      </LiquidGlass>}
+    </>
   );
 }
